9ca7b2412a82efd258318933a7bfcc5cb932fe02
Scorevictim <133180045+SRZhangA@users.noreply.github.com>
2023-05-16 02:27:43 -0700

    add onboarding dbcontext

---
+++ src/Services/OnBoarding/ApplicationCore/Entities/Employee.cs
@@ -0,0 +1,18 @@
+﻿namespace ApplicationCore.Entities;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string Address { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public Guid EmployeeIdeneityId { get; set; }
+    public DateTime EndDate { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public DateTime HireDate { get; set; }
+    public string LastName { get; set; } = string.Empty;
+    public string? MiddleName { get; set; }
+
+    // Navigation
+    public int EmployeeStatusId { get; set; }
+    public EmployeeStatusLookUp EmployeeStatus { get; set; } = new();
+}
---
+++ src/Services/OnBoarding/ApplicationCore/Entities/EmployeeStatusLookUp.cs
@@ -0,0 +1,8 @@
+﻿namespace ApplicationCore.Entities;
+
+public class EmployeeStatusLookUp
+{
+    public int Id { get; set; }
+    public string EmployeeStatusCode { get; set; } = string.Empty;
+    public string EmployeeStatusDescription { get; set; } = string.Empty;
+}
