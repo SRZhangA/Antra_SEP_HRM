d11d6b1e3447d83c2805d98b0c36ed186c512a48
Scorevictim <133180045+SRZhangA@users.noreply.github.com>
2023-06-18 20:34:57 -0700

    init unit tests

---
+++ src/Services/Recruiting/Recruiting.UnitTests/RecruitingServiceUnitTests.cs
@@ -0,0 +1,11 @@
+namespace Recruiting.UnitTests
+{
+    [TestClass]
+    public class RecruitingServiceUnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
