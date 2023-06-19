ed383a7b64eb1629706c9a55cadb8177b73ceab7
Scorevictim <133180045+SRZhangA@users.noreply.github.com>
2023-06-18 22:11:33 -0700

    add moq for unit test

--- src/Services/Recruiting/Recruiting.UnitTests/RecruitingServiceUnitTests.cs
+++ src/Services/Recruiting/Recruiting.UnitTests/RecruitingServiceUnitTests.cs
@@ -1,11 +1,36 @@
+using ApplicationCore.Contracts.Repositories;
+using ApplicationCore.Entities;
+using Infrastructure.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Linq.Expressions;
+
 namespace Recruiting.UnitTests
 {
     [TestClass]
     public class RecruitingServiceUnitTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        private JobService? sut;
+        private readonly Mock<IJobRepository> sutRepository = new();
+        [TestInitialize]
+        public void Setup()
         {
+            sutRepository.Setup(m => m.GetAllAsync()).ReturnsAsync(Enumerable.Empty<Job>());
+            sut = new JobService(sutRepository.Object);
+        }
+        [TestMethod]
+        public async Task TestListJobsFromFakeData()
+        {
+            // AAA: Arrange, Act, and Assert
+
+            // Arrange
+
+            // JobService -> GetAllJobsAsync()
+            // Act
+            var jobs = await sut!.GetAllJobsAsync();
+
+            // Assert
+            Assert.IsNotNull(jobs);
         }
     }
 }
